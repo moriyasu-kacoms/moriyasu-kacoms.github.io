a4f894bd4979ed162f28c7221ea54b1fe67a48ea
守安　悠一 <yuichi.moriyasu@kacoms.co.jp>
2020-09-09 12:45:05 +0900

    JavaScriptに直接クリックイベントを記述

--- EventController.ts
+++ EventController.ts
@@ -1,9 +1,8 @@
-var santaModel = document.getElementById("santa");
-if (santaModel != null){
-    santaModel.onclick = function(){
+
+    document.getElementById("santa").onclick = function(){
+        let santaModel = document.getElementById("santa");
         if (santaModel != null){
-            santaModel.setAttribute("position", "0 0 100");
-            santaModel.setAttribute("rotation", "-90 0 90");
-        }
+           santaModel.setAttribute("position", "0 0 100");
+           santaModel.setAttribute("rotation", "-90 0 90");
+       }
     }
-}
--- output/EventController.js
+++ output/EventController.js
@@ -1,10 +1,9 @@
-var santaModel = document.getElementById("santa");
-if (santaModel != null) {
-    santaModel.onclick = function () {
-        if (santaModel != null) {
-            santaModel.setAttribute("position", "0 0 100");
-            santaModel.setAttribute("rotation", "-90 0 90");
-        }
-    };
-}
+$("#santa").on("click", function()  {
+    var santaModel = document.getElementById("santa");
+    if (santaModel != null) {
+        santaModel.setAttribute("position", "0 0 100");
+        santaModel.setAttribute("rotation", "-90 0 90");
+    }
+});
+
 //# sourceMappingURL=EventController.js.map
