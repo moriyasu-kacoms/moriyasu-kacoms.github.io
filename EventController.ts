74c3e6aeba9113e83654d51c722b9f5774635738
守安　悠一 <yuichi.moriyasu@kacoms.co.jp>
2020-09-09 13:34:32 +0900

    html記述のイベントをonClickに対応

--- EventController.ts
+++ EventController.ts
@@ -1,8 +1,7 @@
-
-    document.getElementById("santa").onclick = function(){
-        let santaModel = document.getElementById("santa");
-        if (santaModel != null){
-           santaModel.setAttribute("position", "0 0 100");
-           santaModel.setAttribute("rotation", "-90 0 90");
-       }
+document.getElementById("santa").onclick = function(){
+    let santaModel = document.getElementById("santa");
+    if (santaModel != null){
+       santaModel.setAttribute("position", "0 0 100");
+       santaModel.setAttribute("rotation", "-90 0 90");
     }
+}
