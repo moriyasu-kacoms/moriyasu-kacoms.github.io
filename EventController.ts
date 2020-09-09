0e1c538c577ced4dee2b1a4a00ff2c31fcf7b479
守安　悠一 <yuichi.moriyasu@kacoms.co.jp>
2020-09-09 10:36:18 +0900

    イベントをTypeScriptで記述

---
+++ EventController.ts
@@ -0,0 +1,9 @@
+var santaModel = document.getElementById("santa");
+if (santaModel != null){
+    santaModel.onclick = function(){
+        if (santaModel != null){
+            santaModel.setAttribute("position", "0 0 100");
+            santaModel.setAttribute("rotation", "-90 0 90");
+        }
+    }
+}
---
+++ output/EventController.js
@@ -0,0 +1,10 @@
+var santaModel = document.getElementById("santa");
+if (santaModel != null) {
+    santaModel.onclick = function () {
+        if (santaModel != null) {
+            santaModel.setAttribute("position", "0 0 100");
+            santaModel.setAttribute("rotation", "-90 0 90");
+        }
+    };
+}
+//# sourceMappingURL=EventController.js.map
