0660bef262b093304d52e402217f9e52fc666cf4
守安　悠一 <yuichi.moriyasu@kacoms.co.jp>
2020-10-09 16:58:29 +0900

    動画再生、3Dモデル表示を追加

---
+++ public/EventController.js
@@ -0,0 +1,22 @@
+AFRAME.registerComponent('markerhandler', {
+    init: function(){
+        const marker = document.getElementById("marker_hiro");
+        const objModel = document.getElementById("santa");
+        const objText = document.getElementById("debug_text");
+
+        if (objText != null){
+            objText.setAttribute('value', "GetElement");
+            //objText.textContent = "GetQuery";
+        }
+
+        marker.addEventListener('click', function(evt, target){
+            const objScale = objModel.getAttribute('scale');
+            Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.5);
+            objModel.setAttribute('scale', objScale);
+
+            objText.setAttribute('value', "Click Event");
+        });
+    }
+});
+
+//# sourceMappingURL=EventController.js.map
