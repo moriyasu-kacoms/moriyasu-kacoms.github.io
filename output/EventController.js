551bae93ff31c9f3693f28f297cf3e7b24a705ca
守安　悠一 <yuichi.moriyasu@kacoms.co.jp>
2020-10-30 14:08:05 +0900

    クリックイベントの導入仕様を変更

--- output/EventController.js
+++ output/EventController.js
@@ -1,17 +1,21 @@
 AFRAME.registerComponent('markerhandler', {
     init: function(){
+        let el = this.el;
         const marker = document.getElementById("marker_hiro");
         const objText = document.getElementById("debug_text");
 
         if (objText != null){
             objText.setAttribute('value', "GetElement");
         }
+
+        //動画オブジェクトにイベントを設定
+        let videoPlayer = document.querySelector('#ar-video');
+        videoPlayer.addEventListener('click', onClick_video(videoPlayer));
     }
 });
 
-//動画クリックイベント
-function onClick_video(){
-    var videoPlayer = document.querySelector('#ar-video');
+ //動画クリックイベント
+function onClick_video(videoPlayer){
     videoPlayer.play();
 }
 
