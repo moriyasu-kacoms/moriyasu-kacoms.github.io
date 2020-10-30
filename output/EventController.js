78d7f30fc53d02450440e7f2a62b06985dc99c1d
守安　悠一 <yuichi.moriyasu@kacoms.co.jp>
2020-10-30 13:22:17 +0900

    未使用の関数を削除し、動画クリックイベントを再度.jsファイルに移動

--- output/EventController.js
+++ output/EventController.js
@@ -1,25 +1,18 @@
 AFRAME.registerComponent('markerhandler', {
     init: function(){
         const marker = document.getElementById("marker_hiro");
-        //const objModel = document.getElementById("santa");
         const objText = document.getElementById("debug_text");
 
         if (objText != null){
             objText.setAttribute('value', "GetElement");
-            //objText.textContent = "GetQuery";
         }
-
-        //markerのクリックイベント
-        marker.addEventListener('click', function(evt, target){
-            //const objScale = objModel.getAttribute('scale');
-            //Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.5);
-            //objModel.setAttribute('scale', objScale);
-
-            objText.setAttribute('value', "Click Event");
-        });
-
-
     }
 });
+
+//動画クリックイベント
+function onClick_video(){
+    var videoPlayer = document.querySelector('#ar-video');
+    videoPlayer.play();
+}
 
 //# sourceMappingURL=EventController.js.map
